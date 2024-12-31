f88f58af38f1ff015b16c0e467927a2e5bedd5d6
Rayane Lamlaika <raynlamlaika.lamlaikarayane@gmail.com>
2024-12-31 21:25:23 +0100

    gjwwwwwwww0

---
+++ NeuralNetowok.cpp
@@ -0,0 +1,31 @@
+//implemment of the neural network
+#include <vector>
+
+class NN
+{
+	private:
+
+
+//need to implemment the nn functions that they gonna be helpfull or the cure of the NN
+	public:
+		NN();
+		//the feedForward function stand for the process of passing the input data through the networks
+		void feedforward(std::vector<double> &inputVal){
+
+			return ;
+		}
+		void backpropagetion(std::vector<double> &targetVal);
+		void theRrsults(std::vector<double> &resultVal) const;
+
+};
+
+
+int main()
+{
+	NN neuralNetwork;
+	int i =0;
+
+
+
+	return i;
+}
